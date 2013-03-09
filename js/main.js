9d2fcf47ed30aa17370cfdfc1b024dd3efca40f1
Mike <mikeybyker@gmail.com>
2013-03-09 16:54:38 +0000

    View Routing

--- js/main.js
+++ js/main.js
@@ -27,14 +27,32 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
+        $(window).on('hashchange', $.proxy(this.route, this));
+    },
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
     },
     initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
             // self.showAlert('Store Initialized', 'Info');
-            $('body').html(new HomeView(self.store).render().el);
+            // $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
-        this.registerEvents();
+        
+        
     }
 
 };
